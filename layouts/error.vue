4d7fc901c16a9a7eb481cd7d437901a7ef14dc10
mendoitarou <42207304+mendoitarou@users.noreply.github.com>
2025-06-03 23:21:35 +0900

    Add_Error-Page

---
+++ layouts/error.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="error">
+        <div>
+            <p v-if="status === 404">
+                404 Not Found<br>
+            </p>
+            <p v-else>Error: {{ status }}</p>
+            <NuxtLink to="/">Return Top</NuxtLink>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        error: {
+            type: Object,
+            default: null
+        }
+    },
+    computed: {
+        status() {
+            return this.error.statusCode;
+        }
+    }
+};
+</script>
