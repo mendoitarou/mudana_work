dd70b70e1482e0f5146c0d65b476088839e48242
mendoitarou <42207304+mendoitarou@users.noreply.github.com>
2025-06-03 23:09:41 +0900

    Add_Site-Contents

--- app.vue
+++ app.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div class="main">
+      無駄なWork
+    </div>
+    <div class="twitter_share_button">
+      <Twitter_Share />
+    </div>
+    <div class="copyright">
+      <small><NuxtLink to="https://home.mendoitarou.com/">mendoitarou_</NuxtLink></small>
+    </div>
   </div>
 </template>
+
+<style>
+  .main {
+    font-size: xx-large;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  
+  .copyright {
+    position: absolute;
+    right: 5px;
+    bottom: 0px;
+  }
+
+  .twitter_share_button {
+    position: absolute;
+    left: 5px;
+    bottom: 0px;
+  }
+</style>
---
+++ components/Twitter_Share.vue
@@ -0,0 +1,19 @@
+<template>
+    <span>
+        <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="無駄なwork" data-url="https://mudana.work/" data-show-count="false">Tweet</a>
+    </span>
+</template>
+
+<script>
+export default {
+    mounted () {
+        const script = document.createElement('script');
+        script.src = 'https://platform.twitter.com/widgets.js';
+        script.async = true;
+        document.body.appendChild(script);
+        return () => {
+            document.body.removeChild(script);
+        }
+    }
+}
+</script>
